ba65140cf857345f2bd10fb6653cd6f8c78e0211
noidfrx <edufco9@gmail.com>
2021-01-10 01:37:04 -0300

    Se Crea recyclerView, se llena y se utiliza picasso para trato de imágenes

--- app/src/main/java/com/noidfrx/aprendiendo_android_s6_fruitworld2/activities/MainActivity.java
+++ app/src/main/java/com/noidfrx/aprendiendo_android_s6_fruitworld2/activities/MainActivity.java
@@ -2,21 +2,63 @@ package com.noidfrx.aprendiendo_android_s6_fruitworld2.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
 import com.noidfrx.aprendiendo_android_s6_fruitworld2.R;
+import com.noidfrx.aprendiendo_android_s6_fruitworld2.adapters.MyAdapter;
+import com.noidfrx.aprendiendo_android_s6_fruitworld2.models.Fruta;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Fruta> listaDeFrutas;
+    private RecyclerView rvFrutas;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inicializaListaDeFrutas();
+
+        rvFrutas = findViewById(R.id.rvFrutas);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new MyAdapter( listaDeFrutas, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {
+
+            /*  Lo que se hará al hacer click a un item   */
+            @Override
+            public void onItemClick(String nombre, String descripcion, int imgBackground, int imgIcon, int cantidad, int position) {
+                Toast.makeText(MainActivity.this, "Esto es: "+nombre+" Posición: "+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        rvFrutas.setLayoutManager(mLayoutManager);
+        rvFrutas.setAdapter(mAdapter);
+
+        registerForContextMenu(rvFrutas);
+    }
+
+    private void inicializaListaDeFrutas() {
+        listaDeFrutas = new ArrayList<>();
+        listaDeFrutas.add(new Fruta("Strawberry", "Strawberry description", R.drawable.strawberry_bg, R.drawable.ic_strawberry, 0));
+        listaDeFrutas.add(new Fruta("Strawberry", "Strawberry description", R.drawable.strawberry_bg, R.drawable.ic_strawberry, 0));
+        listaDeFrutas.add(new Fruta("Strawberry", "Strawberry description", R.drawable.strawberry_bg, R.drawable.ic_strawberry, 0));
+        listaDeFrutas.add(new Fruta("Strawberry", "Strawberry description", R.drawable.strawberry_bg, R.drawable.ic_strawberry, 0));
+        listaDeFrutas.add(new Fruta("Strawberry", "Strawberry description", R.drawable.strawberry_bg, R.drawable.ic_strawberry, 0));
+        listaDeFrutas.add(new Fruta("Strawberry", "Strawberry description", R.drawable.strawberry_bg, R.drawable.ic_strawberry, 0));
+
     }
 
     @Override
@@ -39,4 +81,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/noidfrx/aprendiendo_android_s6_fruitworld2/adapters/MyAdapter.java
@@ -0,0 +1,97 @@
+package com.noidfrx.aprendiendo_android_s6_fruitworld2.adapters;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.noidfrx.aprendiendo_android_s6_fruitworld2.R;
+import com.noidfrx.aprendiendo_android_s6_fruitworld2.models.Fruta;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<Fruta> listaDeFrutas;
+    private int layout;
+    private OnItemClickListener itemClickListener;
+    private Context context;
+
+    public MyAdapter(List<Fruta> listaDeFrutas, int layout, OnItemClickListener itemClickListener) {
+        this.listaDeFrutas = listaDeFrutas;
+        this.layout = layout;
+        this.itemClickListener = itemClickListener;
+    }
+
+
+    @NonNull
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(layout,parent,false);
+        context = parent.getContext();
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+        holder.bind(listaDeFrutas.get(position),itemClickListener, context);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaDeFrutas.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView ivFruta;
+        private TextView tvNombre,tvDescripcion,tvCantidad;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivFruta = itemView.findViewById(R.id.ivFruta);
+            tvNombre = itemView.findViewById(R.id.tvNombre);
+            tvDescripcion = itemView.findViewById(R.id.tvDescripcion);
+            tvCantidad = itemView.findViewById(R.id.tvCantidad);
+
+        }
+
+        public void bind(Fruta fruta, OnItemClickListener listener, Context context){
+            Picasso.with(context).load(fruta.getImgBackground()).into(ivFruta);
+            tvNombre.setText(fruta.getNombre());
+            tvDescripcion.setText(fruta.getDescripcion());
+            tvCantidad.setText(""+fruta.getCantidad());
+
+
+            /*  Se entregan valores hacia el main para cuando interfaz se instancie allá    */
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(fruta.getNombre(),fruta.getDescripcion(),fruta.getImgBackground(),fruta.getImgIcon(),fruta.getCantidad(),getAdapterPosition());
+                }
+            });
+
+        }
+
+
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(String nombre, String descripcion, int imgBackground, int imgIcon, int cantidad, int position);
+    }
+
+
+
+}
+
+
